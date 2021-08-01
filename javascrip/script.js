c213dab499af578e0ea81c77ca9ff8ae2711e448
Fernando <fernandodevpascoal@gmail.com>
2021-08-01 14:00:32 -0300

    Primeiro Commit. Projeto estruturado e já com edições

---
+++ javascrip/script.js
@@ -0,0 +1,32 @@
+
+
+
+function capa1() {
+  remove()
+  document.querySelector('.slidershow').classList.add('item1');
+
+}
+
+function capa2() {
+  remove()
+  document.querySelector('.slidershow').classList.add('item2');
+
+}
+
+function capa3() {
+  remove()
+  document.querySelector('.slidershow').classList.add('item3');
+
+}
+
+function remove() {
+  document.querySelector('.slidershow').classList.remove('item1')
+  document.querySelector('.slidershow').classList.remove('item2')
+  document.querySelector('.slidershow').classList.remove('item3')
+}
+
+
+function rightarrow() {
+
+}
+
