09f651c84941a199663b368d5fff51537017df4a
Fernando <fernandodevpascoal@gmail.com>
2021-08-01 15:12:57 -0300

    Estrutura Colapse

--- javascrip/script.js
+++ javascrip/script.js
@@ -30,3 +30,30 @@ function rightarrow() {
 
 }
 
+
+function mostrar1() {
+  let close = document.querySelectorAll(".colapse_content")[0].style.display = "none";
+  let open = document.querySelectorAll(".colapse_content")[0].style.display = "flex";
+
+  if (close) {
+    document.querySelectorAll(".colapse_content")[0].style.display = "flex";
+    document.querySelectorAll(".colapse_menu")[0].style.background = "white";
+    document.querySelectorAll(".colapse_menu")[0].style.border_radius = "5px";
+    document.querySelectorAll(".colapse_title")[0].style.color = "black";
+    document.querySelectorAll(".colapse.icon")[0].setAttribute('src', 'images/uparrow.png');
+  } else {
+    document.querySelectorAll(".colapse_content")[0].style.display = "none";
+  }
+}
+
+function mostrar2() {
+  document.querySelectorAll(".colapse_content")[1].style.display = "flex";
+}
+
+function mostrar3() {
+  document.querySelectorAll(".colapse_content")[2].style.display = "flex";
+}
+
+function mostrar4() {
+  document.querySelectorAll(".colapse_content")[3].style.display = "flex";
+}
